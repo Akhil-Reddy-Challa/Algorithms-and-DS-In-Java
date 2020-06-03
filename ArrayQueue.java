33492035360dab6511331b24f12d8095337014a4
AkhilReddy1998 <akhilreddy.challa98@gmail.com>
2020-06-03 11:59:42 +0530

    Checking_no_changes

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -6,7 +6,7 @@ public class ArrayQueue {
     int rear = -1;
     int[] array_for_queue = new int[100];
     int current_size = 0;
-    
+    int tt=0;
 	public void enqueue(int number) {
         if(current_size < 100){
             array_for_queue[current_size++] = number;
