ea67ff564442b84802dbde55d80f271cd3d547d3
Akhil Reddy <akhilreddychalla98@gmail.com>
2020-06-04 10:47:02 +0530

    Implemented_insert_with_recursion

--- AVLTrees.java
+++ AVLTrees.java
@@ -37,11 +37,23 @@ public class AVLTrees {
     public void displayInorder(){
         displayInorder(root);
     }
-
     private void displayInorder(AVLNode root) {
         if(root == null) return;
         displayInorder(root.leftChild);
         System.out.println(root.data);
         displayInorder(root.rightChild);
     }
+	public void insertWithRecursion(int data) {
+        root = insertWithRecursion(root,data);
+    }
+    private AVLNode insertWithRecursion(AVLNode temp_root, int data) {
+        if(temp_root == null) 
+            return new AVLNode(data);
+        if(data > temp_root.data)
+            root.rightChild = insertWithRecursion(temp_root.rightChild, data);
+        else if(data < root.data)
+            root.leftChild = insertWithRecursion(temp_root.leftChild, data);
+        return root;
+    }
+
 }
--- ProgramForAVLTrees.java
+++ ProgramForAVLTrees.java
@@ -1,9 +1,13 @@
 public class ProgramForAVLTrees {
     public static void main(String[] args) {
         AVLTrees avl = new AVLTrees();
-        avl.insert(12);
-        avl.insert(2);
-        avl.insert(20);
+        //avl.insert(12);
+        avl.insertWithRecursion(12);
+        avl.insertWithRecursion(2);
+        avl.insertWithRecursion(20);
         avl.displayInorder();
+        ///Implement Height checker feature
+        // Introduce new Integer height in AVLNode class and store height
+        // as you insert elements;
     }
 }
