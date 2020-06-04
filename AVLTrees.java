81b043fd1ae9fab70a21456c472538471d6d5eb0
Akhil Reddy <akhilreddychalla98@gmail.com>
2020-06-04 10:20:07 +0530

    Insert_function_with_non_recursion

--- AVLTrees.java
+++ AVLTrees.java
@@ -1,3 +1,47 @@
 public class AVLTrees {
-    
+    private class AVLNode{
+        int data;
+        AVLNode leftChild;
+        AVLNode rightChild;
+        AVLNode(int data){
+            this.data = data;
+        }
+    }
+    private AVLNode root;
+	public void insert(int data){
+        if(root == null){
+            root = new AVLNode(data);
+        }
+        else{
+            AVLNode temp = root;
+            while(temp!=null){
+                if(data > temp.data){
+                    if(temp.rightChild == null){
+                        temp.rightChild = new AVLNode(data);
+                        break;
+                    }
+                    else temp = temp.rightChild;
+                }
+                else{
+                    if(data < temp.data){
+                        if(temp.leftChild == null){
+                            temp.leftChild = new AVLNode(data);
+                            break;
+                        }
+                        else temp = temp.leftChild;
+                    }
+                }
+            }
+        }
+    }
+    public void displayInorder(){
+        displayInorder(root);
+    }
+
+    private void displayInorder(AVLNode root) {
+        if(root == null) return;
+        displayInorder(root.leftChild);
+        System.out.println(root.data);
+        displayInorder(root.rightChild);
+    }
 }
---
+++ ProgramForAVLTrees.java
@@ -0,0 +1,9 @@
+public class ProgramForAVLTrees {
+    public static void main(String[] args) {
+        AVLTrees avl = new AVLTrees();
+        avl.insert(12);
+        avl.insert(2);
+        avl.insert(20);
+        avl.displayInorder();
+    }
+}
