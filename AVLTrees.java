40c74c5c3f959e1e84ffdeb5ff70b6f818b91736
AkhilReddy1998 <akhilreddy.challa98@gmail.com>
2020-06-05 18:41:44 +0530

    Implemented_Height_Functionality_for_tree

--- AVLTrees.java
+++ AVLTrees.java
@@ -3,6 +3,7 @@ public class AVLTrees {
         int data;
         AVLNode leftChild;
         AVLNode rightChild;
+        int height;
         AVLNode(int data){
             this.data = data;
         }
@@ -40,20 +41,34 @@ public class AVLTrees {
     private void displayInorder(AVLNode root) {
         if(root == null) return;
         displayInorder(root.leftChild);
-        System.out.println(root.data);
+        System.out.println(root.data + " Height: "+root.height);
         displayInorder(root.rightChild);
     }
 	public void insertWithRecursion(int data) {
         root = insertWithRecursion(root,data);
     }
     private AVLNode insertWithRecursion(AVLNode root, int data) {
-        if(root == null) 
+        if(root == null)
             return new AVLNode(data);
         if(data > root.data)
             root.rightChild = insertWithRecursion(root.rightChild, data);
         else if(data < root.data)
             root.leftChild = insertWithRecursion(root.leftChild, data);
+        
+        root.height = Math.max(GetLeftHeight(root),GetRightHeight(root)) + 1;
         return root;
+    }
+
+    private int GetRightHeight(AVLNode root) {
+        if(root.rightChild == null)
+            return 0;
+        else return root.rightChild.height;
+    }
+
+    private int GetLeftHeight(AVLNode root) {
+        if(root.leftChild == null)
+            return 0;
+        else return root.leftChild.height;
     }
 
 }
--- ProgramForAVLTrees.java
+++ ProgramForAVLTrees.java
@@ -6,9 +6,13 @@ public class ProgramForAVLTrees {
         avl.insertWithRecursion(2);
         avl.insertWithRecursion(20);
         avl.insertWithRecursion(1);
+        avl.insertWithRecursion(3);
+        avl.insertWithRecursion(18);
+        avl.insertWithRecursion(22);
         avl.displayInorder();
         ///Implement Height checker feature
         // Introduce new Integer height in AVLNode class and store height
         // as you insert elements;
+
     }
 }
