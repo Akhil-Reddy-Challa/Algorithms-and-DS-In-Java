28899cfdfa17d80b1518cd51a6d0914eaec3ad12
AkhilReddy1998 <akhilreddy.challa98@gmail.com>
2020-06-05 20:13:59 +0530

    Implemented_balance_factor

--- AVLTrees.java
+++ AVLTrees.java
@@ -10,12 +10,15 @@ public class AVLTrees {
     }
     private AVLNode root;
     public void displayInorder(){
+        System.out.print("[");
         displayInorder(root);
+        System.out.println("]");
     }
     private void displayInorder(AVLNode root) {
         if(root == null) return;
         displayInorder(root.leftChild);
-        System.out.println(root.data + " Height: "+root.height);
+        //System.out.print("Data: "+root.data + " Height: "+root.height + " , ");
+        System.out.print(root.data+" : "+root.height+" ,");
         displayInorder(root.rightChild);
     }
 	public void insertWithRecursion(int data) {
@@ -30,10 +33,23 @@ public class AVLTrees {
             root.leftChild = insertWithRecursion(root.leftChild, data);
         
         root.height = Math.max(height(root.leftChild),height(root.rightChild)) + 1;
+        balanceFactor();
         return root;
+    }
+    private void balanceFactor() {
+        int leftTreeHeight = height(root.leftChild);
+        int rightTreeHeight = height(root.rightChild);
+        int difference_of_height = leftTreeHeight - rightTreeHeight;
+        if(difference_of_height>1)
+            System.out.println("Left hanging tree");
+        else if(difference_of_height < -1)
+            System.out.println("Right Hanging tree");
+        else
+            System.out.println("Perfectly balanced tree");
+        displayInorder();
     }
 
     private int height(AVLNode root) {
-        return root == null ? 0: root.height;
+        return root == null ? -1: root.height;
     }
 }
--- ProgramForAVLTrees.java
+++ ProgramForAVLTrees.java
@@ -1,15 +1,13 @@
 public class ProgramForAVLTrees {
     public static void main(String[] args) {
         AVLTrees avl = new AVLTrees();
-        //avl.insert(12);
-        avl.insertWithRecursion(12);
-        avl.insertWithRecursion(2);
+
+        avl.insertWithRecursion(10);
         avl.insertWithRecursion(20);
-        avl.insertWithRecursion(1);
-        avl.insertWithRecursion(3);
-        avl.insertWithRecursion(18);
-        avl.insertWithRecursion(22);
-        avl.displayInorder();
+        avl.insertWithRecursion(30);
+        //avl.insertWithRecursion(6);
+        //avl.insertWithRecursion(22);
+        //avl.displayInorder();
         ///Implement Height checker feature
         // Introduce new Integer height in AVLNode class and store height
         // as you insert elements;
