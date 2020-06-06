0e1031e4bd80054d40913f3a330bb779f8b205b3
Akhil Reddy <akhilreddychalla98@gmail.com>
2020-06-06 14:27:19 +0530

    1) Modification_To_Insert_method 2) Introduced_balanceFactor 3) It finds if tree is not balanced 4) Enhancement_to_Inorder_tranversal_method

--- AVLTrees.java
+++ AVLTrees.java
@@ -10,46 +10,36 @@ public class AVLTrees {
     }
     private AVLNode root;
     public void displayInorder(){
-        System.out.print("[");
         displayInorder(root);
-        System.out.println("]");
     }
     private void displayInorder(AVLNode root) {
         if(root == null) return;
         displayInorder(root.leftChild);
-        //System.out.print("Data: "+root.data + " Height: "+root.height + " , ");
-        System.out.print(root.data+" : "+root.height+" ,");
+        System.out.print(root.data + ":"+root.height + " ");
         displayInorder(root.rightChild);
     }
-	public void insertWithRecursion(int data) {
-        root = insertWithRecursion(root,data);
+	public void insert(int data) {
+        root = insert(root,data);
     }
-    private AVLNode insertWithRecursion(AVLNode root, int data) {
+    private AVLNode insert(AVLNode root, int data) {
         if(root == null)
             return new AVLNode(data);
         if(data > root.data)
-            root.rightChild = insertWithRecursion(root.rightChild, data);
+            root.rightChild = insert(root.rightChild, data);
         else if(data < root.data)
-            root.leftChild = insertWithRecursion(root.leftChild, data);
+            root.leftChild = insert(root.leftChild, data);
         
         root.height = Math.max(height(root.leftChild),height(root.rightChild)) + 1;
-        balanceFactor();
+        /// Below is for balancing Tree
+        var balanceFactor = height(root.leftChild) - height(root.rightChild);
+        if(balanceFactor>1)
+            System.out.println("Left hanging tree at: " + root.data);
+        else if(balanceFactor < -1)
+            System.out.println("Right Hanging tree at: "+ root.data);
         return root;
     }
-    private void balanceFactor() {
-        int leftTreeHeight = height(root.leftChild);
-        int rightTreeHeight = height(root.rightChild);
-        int difference_of_height = leftTreeHeight - rightTreeHeight;
-        if(difference_of_height>1)
-            System.out.println("Left hanging tree");
-        else if(difference_of_height < -1)
-            System.out.println("Right Hanging tree");
-        else
-            System.out.println("Perfectly balanced tree");
-        displayInorder();
-    }
-
     private int height(AVLNode root) {
         return root == null ? -1: root.height;
     }
+
 }
--- ProgramForAVLTrees.java
+++ ProgramForAVLTrees.java
@@ -2,15 +2,10 @@ public class ProgramForAVLTrees {
     public static void main(String[] args) {
         AVLTrees avl = new AVLTrees();
 
-        avl.insertWithRecursion(10);
-        avl.insertWithRecursion(20);
-        avl.insertWithRecursion(30);
-        //avl.insertWithRecursion(6);
-        //avl.insertWithRecursion(22);
-        //avl.displayInorder();
-        ///Implement Height checker feature
-        // Introduce new Integer height in AVLNode class and store height
-        // as you insert elements;
+        avl.insert(10);
+        avl.insert(30);
+        avl.insert(20);
+        
 
     }
 }
