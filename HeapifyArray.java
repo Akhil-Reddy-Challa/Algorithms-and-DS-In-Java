5e6e7ac7074619a13c2d28db9cff0ade753a1e9c
AkhilReddy1998 <akhilreddychalla98@gmail.com>
2021-06-11 17:51:47 -0400

    should implement

--- HeapifyArray.java
+++ HeapifyArray.java
@@ -28,24 +28,33 @@ public class HeapifyArray {
         return highestNumber;
     }
 
-    /*
-     * private static void heapify(int[] numbers, int position, int length) {
-     * 
-     * int highIndex = position;//Assume parent is highest //Now see if leftChild
-     * exists, if yes compare value int leftChildIndex = 2*position +1;
-     * if(leftChildIndex < length && numbers[leftChildIndex] > numbers[highIndex])
-     * highIndex = leftChildIndex;//LeftChild is greater so highIndex points to
-     * leftChild int rightChildIndex = 2*position+2; if(rightChildIndex < length &&
-     * numbers[rightChildIndex] > numbers[highIndex]) highIndex =
-     * rightChildIndex;//Right child exists and is greater than highIndex(leftChild
-     * or parent)
-     * 
-     * if(position == highIndex) return; swap(numbers, position, highIndex);
-     * heapify(numbers,highIndex, length);
-     * 
-     * }
-     * 
-     * private static void swap(int[] numbers, int i, int j) { int temp =
-     * numbers[i]; numbers[i] = numbers[j]; numbers[j] = temp; }
-     */
+    // private static void heapify(int[] numbers, int position, int length) {
+
+    // int highIndex = position;// Assume parent is highest
+    // // Now see if leftChild exists, if yes compare value
+    // int leftChildIndex = 2 * position + 1;
+
+    // if (leftChildIndex < length && numbers[leftChildIndex] > numbers[highIndex])
+    // highIndex = leftChildIndex;// LeftChild is greater so highIndex points to
+    // leftChild
+    // int rightChildIndex = 2 * position + 2;
+
+    // if (rightChildIndex < length && numbers[rightChildIndex] >
+    // numbers[highIndex])
+    // highIndex = rightChildIndex;// Right child exists and is greater than
+    // highIndex(leftChild or parent)
+
+    // if (position == highIndex)
+    // return;
+    // swap(numbers, position, highIndex);
+    // heapify(numbers, highIndex, length);
+
+    // }
+
+    // private static void swap(int[] numbers, int i, int j) {
+    // int temp = numbers[i];
+    // numbers[i] = numbers[j];
+    // numbers[j] = temp;
+    // }
+
 }
